080ba958d29ff0cacdae1cb0e65c8a3a95253717
Brandon Keepers <brandon@opensoul.org>
2026-02-03 08:49:48 -0500

    [api] Add compression support

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import compression from "compression";
 import routes from "./routes.js";
 import openapi from "./openapi.js";
 
 export function createApp() {
-  return express().use("/", routes);
+  const app = express();
+  app.use(compression());
+  app.use("/", routes);
+  return app;
 }
 
 export { routes, openapi };
